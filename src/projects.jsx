3ca5c77bc0e7554e8be69ab0dfb4016fdf7a246d
phenomenalCode <gbreal50@gmail.com>
2025-09-14 18:10:35 +0200

    updated portfolio

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 
 import img1 from './images/reconomy-logo.png';
-import img2 from './images/unnamed.jpg';
+import img2 from './images/IMG_3828.jpeg';
 import img3 from './images/weatherapps-android-1weather-5b751ae9c9e77c0057fc06d5.jpg';
+import img4 from './images/IMG_3849.jpeg';
 export const projectData = [
   {
     id: 1,
     title: 'Reconomy Check-In App',
-    description: 'Check-in system and admin page designed for Reconomy AB.',
+    description: 'Check-in system and admin dashboard designed for Reconomy AB. A full-stack employee time-tracking system built with Node.js, Express, and MySQL, featuring Employee Management , Create, update, and filter employee records Time Logging , Log check-in/out events with optional comments Admin Login , Session-based authentication for protected routes Cross-Origin Support , Fully functional CORS setup for Netlify Heroku deployment, Backend hosted on Heroku, frontend on Netlify',
     img: img1,
-    github: 'https://github.com/your-username/reconomy-checkin',
+    github: 'https://github.com/phenomenalCode/Reconomy',
   },
   {
     id: 2,
-    title: 'Velita Clinique Booking',
-    description: 'Booking form, admin page & check-in page for Velita Clinique.',
+    title: 'Happy Thoughts',
+    description: 'Happy Thoughts is a full-stack web app where users can post uplifting messages, like others thoughts, and manage their own posts. It includes user authentication, random thought display, and like tracking.Tools used: React, MUI, Node.js, Express, MongoDB (Mongoose), JWT, Netlify, Render.',
     img: img2,
-    github: 'https://github.com/your-username/velita-clinique',
+    github: 'https://github.com/phenomenalCode/js-project-happy-thoughts/tree/happywbackend',
   },
   {
     id: 3,
     title: 'Weather App',
-    description: 'A simple weather forecast app.',
+    description: 'A simple weather forecast app. It displays current weather and a 4-day forecast using the OpenWeatherMap API. Users can search cities or cycle through presets (Stockholm, Gothenburg, Oslo). It shows temperature, weather icons, sunrise/sunset times, and updates the background dynamically based on conditions. Weather data is cached using localStorage.',
     img: img3,
     github: 'https://github.com/phenomenalCode/weather-app',
   },
+ {
+  id: 4,
+  title: 'Task Manager',
+  description: 'A full-stack task and project management app with authentication, group collaboration, and file uploads. Built with React (Vite), Zustand, and Material-UI on the frontend, and Node.js, Express, and MongoDB with GridFS on the backend. Features include dark mode, filtering, sorting, and real-time project completion tracking.',
+  img: img4,
+  github: 'https://github.com/phenomenalCode/project-final-darius/tree/main',
+},
+
 ];
 
 
