04589a3ca6669b9128fe3018b4321824ebca1c8a
phenomenalCode <gbreal50@gmail.com>
2025-12-29 18:56:08 +0100

    Add all project files

--- src/projects.jsx
+++ src/projects.jsx
@@ -4,6 +4,7 @@ import img1 from './images/reconomy-logo.png';
 import img2 from './images/IMG_3828.jpeg';
 import img3 from './images/weatherapps-android-1weather-5b751ae9c9e77c0057fc06d5.jpg';
 import img4 from './images/IMG_3849.jpeg';
+import img5 from './images/musicrewardsphoto.jpg';
 export const projectData = [
   {
     id: 1,
@@ -32,6 +33,12 @@ export const projectData = [
   description: 'A full-stack task and project management app with authentication, group collaboration, and file uploads. Built with React (Vite), Zustand, and Material-UI on the frontend, and Node.js, Express, and MongoDB with GridFS on the backend. Features include dark mode, filtering, sorting, and real-time project completion tracking.',
   img: img4,
   github: 'https://github.com/phenomenalCode/project-final-darius/tree/main',
+},{
+  id: 5,
+  title: 'Music Rewards App',
+  description: 'MusicRewards is a React Native (Expo) app that provides short music "challenges" (listen to tracks to earn points). It\'s implemented with Expo and React Native, uses Zustand for local app state with AsyncStorage persistence, and expo-av for audio playback. UI is componentized into challenge cards, lists, and small UI primitives (GlassButton/GlassCard) with a central theme tokens file.',
+  img: img5,
+  github: 'https://github.com/phenomenalCode/Darius-Music-Reward-App/tree/main',
 },
 
 ];
